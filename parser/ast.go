fb9825bb9318cf5a657fdec81d4fd46f142e638a
Akash <akashsivanandan@protonmail.com>
2026-01-09 18:47:38 -0500

    feat: add position tracking and expose a Position() method to the expression interface

--- parser/ast.go
+++ parser/ast.go
@@ -6,12 +6,12 @@ type Operator int
 
 type Position struct {
 	Line int
-	Col int
+	Col  int
 }
 
 type Range struct {
 	Start Position
-	End Position
+	End   Position
 }
 
 const (
@@ -32,35 +32,39 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+	Position() Range
 }
 
 type Number struct {
 	Value string
-	Pos Range
+	Pos   Range
 }
 
 func (n *Number) expressionNode() {}
+func (n *Number) Position() Range { return n.Pos }
 
 type Name struct {
-	Id string
+	Id  string
 	Pos Range
 }
 
 func (n *Name) expressionNode() {}
+func (n *Name) Position() Range { return n.Pos }
 
 type BinOp struct {
 	Left  Expression
 	Op    Operator
 	Right Expression
-	Pos Range
+	Pos   Range
 }
 
 func (b *BinOp) expressionNode() {}
+func (b *BinOp) Position() Range { return b.Pos }
 
 type Assign struct {
 	Targets []Expression
 	Value   Expression
-	Pos Range
+	Pos     Range
 }
 
 func (a *Assign) statementNode() {}
@@ -72,21 +76,21 @@ type Module struct {
 type FuncArg struct {
 	Name    string
 	Default Expression
-	Pos Range
+	Pos     Range
 }
 
 type FunctionDef struct {
 	Name string
 	Args []FuncArg
 	Body []Statement
-	Pos Range
+	Pos  Range
 }
 
 func (f *FunctionDef) statementNode() {}
 
 type Return struct {
 	Value Expression
-	Pos Range
+	Pos   Range
 }
 
 func (r *Return) statementNode() {}
@@ -95,23 +99,24 @@ type If struct {
 	Test   Expression
 	Body   []Statement
 	Orelse []Statement
-	Pos Range
+	Pos    Range
 }
 
 func (i *If) statementNode() {}
 
 type Tuple struct {
 	Elts []Expression
-	Pos Range
+	Pos  Range
 }
 
 func (t *Tuple) expressionNode() {}
+func (t *Tuple) Position() Range { return t.Pos }
 
 type For struct {
 	Target Expression
 	Iter   Expression
 	Body   []Statement
-	Pos Range
+	Pos    Range
 }
 
 func (f *For) statementNode() {}
@@ -119,7 +124,7 @@ func (f *For) statementNode() {}
 type WhileLoop struct {
 	Test Expression
 	Body []Statement
-	Pos Range
+	Pos  Range
 }
 
 func (w *WhileLoop) statementNode() {}
@@ -127,19 +132,21 @@ func (w *WhileLoop) statementNode() {}
 type Call struct {
 	Func Expression
 	Args []Expression
-	Pos Range
+	Pos  Range
 }
 
 func (c *Call) expressionNode() {}
+func (c *Call) Position() Range { return c.Pos }
 
 type Compare struct {
 	Left  Expression
 	Ops   []CompareOp
 	Right []Expression
-	Pos Range
+	Pos   Range
 }
 
 func (c *Compare) expressionNode() {}
+func (c *Compare) Position() Range { return c.Pos }
 
 type CompareOp int
 
@@ -160,35 +167,37 @@ const (
 	Not                       // not x
 )
 
-
 type UnaryOp struct {
 	Op      UnaryOperator
 	Operand Expression
-	Pos Range
+	Pos     Range
 }
 
 func (u *UnaryOp) expressionNode() {}
+func (u *UnaryOp) Position() Range { return u.Pos }
 
 type String struct {
 	Value string
-	Pos Range
+	Pos   Range
 }
 
 func (s *String) expressionNode() {}
+func (s *String) Position() Range { return s.Pos }
 
 type ExprStmt struct {
 	Value Expression
-	Pos Range
+	Pos   Range
 }
 
 func (e *ExprStmt) statementNode() {}
 
 type Boolean struct {
 	Value bool
-	Pos Range
+	Pos   Range
 }
 
 func (b *Boolean) expressionNode() {}
+func (b *Boolean) Position() Range { return b.Pos }
 
 type BooleanOperator int
 
@@ -200,25 +209,27 @@ const (
 type BooleanOp struct {
 	Operator BooleanOperator
 	Values   []Expression
-	Pos Range
+	Pos      Range
 }
 
 func (bo *BooleanOp) expressionNode() {}
+func (bo *BooleanOp) Position() Range { return bo.Pos }
 
 type List struct {
 	Elts []Expression
-	Pos Range
+	Pos  Range
 }
 
 func (l *List) expressionNode() {}
+func (l *List) Position() Range { return l.Pos }
 
-type Break struct{
+type Break struct {
 	Pos Range
 }
 
 func (b *Break) statementNode() {}
 
-type Continue struct{
+type Continue struct {
 	Pos Range
 }
 
