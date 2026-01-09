afae9117ed1daede5b14d2986bebdc2e12bb6ade
Akash <akashsivanandan@protonmail.com>
2026-01-09 17:33:54 -0500

    feat: add start and end pos tracking to all AST nodes

--- parser/ast.go
+++ parser/ast.go
@@ -4,6 +4,16 @@ type Node any
 
 type Operator int
 
+type Position struct {
+	Line int
+	Col int
+}
+
+type Range struct {
+	Start Position
+	End Position
+}
+
 const (
 	Add Operator = iota
 	Sub
@@ -26,12 +36,14 @@ type Expression interface {
 
 type Number struct {
 	Value string
+	Pos Range
 }
 
 func (n *Number) expressionNode() {}
 
 type Name struct {
 	Id string
+	Pos Range
 }
 
 func (n *Name) expressionNode() {}
@@ -40,6 +52,7 @@ type BinOp struct {
 	Left  Expression
 	Op    Operator
 	Right Expression
+	Pos Range
 }
 
 func (b *BinOp) expressionNode() {}
@@ -47,6 +60,7 @@ func (b *BinOp) expressionNode() {}
 type Assign struct {
 	Targets []Expression
 	Value   Expression
+	Pos Range
 }
 
 func (a *Assign) statementNode() {}
@@ -58,18 +72,21 @@ type Module struct {
 type FuncArg struct {
 	Name    string
 	Default Expression
+	Pos Range
 }
 
 type FunctionDef struct {
 	Name string
 	Args []FuncArg
 	Body []Statement
+	Pos Range
 }
 
 func (f *FunctionDef) statementNode() {}
 
 type Return struct {
 	Value Expression
+	Pos Range
 }
 
 func (r *Return) statementNode() {}
@@ -78,12 +95,14 @@ type If struct {
 	Test   Expression
 	Body   []Statement
 	Orelse []Statement
+	Pos Range
 }
 
 func (i *If) statementNode() {}
 
 type Tuple struct {
 	Elts []Expression
+	Pos Range
 }
 
 func (t *Tuple) expressionNode() {}
@@ -92,6 +111,7 @@ type For struct {
 	Target Expression
 	Iter   Expression
 	Body   []Statement
+	Pos Range
 }
 
 func (f *For) statementNode() {}
@@ -99,6 +119,7 @@ func (f *For) statementNode() {}
 type WhileLoop struct {
 	Test Expression
 	Body []Statement
+	Pos Range
 }
 
 func (w *WhileLoop) statementNode() {}
@@ -106,6 +127,7 @@ func (w *WhileLoop) statementNode() {}
 type Call struct {
 	Func Expression
 	Args []Expression
+	Pos Range
 }
 
 func (c *Call) expressionNode() {}
@@ -114,6 +136,7 @@ type Compare struct {
 	Left  Expression
 	Ops   []CompareOp
 	Right []Expression
+	Pos Range
 }
 
 func (c *Compare) expressionNode() {}
@@ -137,29 +160,32 @@ const (
 	Not                       // not x
 )
 
-func (u *UnaryOperator) expressionNode() {}
 
 type UnaryOp struct {
 	Op      UnaryOperator
 	Operand Expression
+	Pos Range
 }
 
 func (u *UnaryOp) expressionNode() {}
 
 type String struct {
 	Value string
+	Pos Range
 }
 
 func (s *String) expressionNode() {}
 
 type ExprStmt struct {
 	Value Expression
+	Pos Range
 }
 
 func (e *ExprStmt) statementNode() {}
 
 type Boolean struct {
 	Value bool
+	Pos Range
 }
 
 func (b *Boolean) expressionNode() {}
@@ -174,20 +200,26 @@ const (
 type BooleanOp struct {
 	Operator BooleanOperator
 	Values   []Expression
+	Pos Range
 }
 
 func (bo *BooleanOp) expressionNode() {}
 
 type List struct {
 	Elts []Expression
+	Pos Range
 }
 
 func (l *List) expressionNode() {}
 
-type Break struct{}
+type Break struct{
+	Pos Range
+}
 
 func (b *Break) statementNode() {}
 
-type Continue struct{}
+type Continue struct{
+	Pos Range
+}
 
 func (c *Continue) statementNode() {}
