cd58caaec9a6e9bd5995cfc290f66c2d42f336fd
Akash <akashsivanandan@protonmail.com>
2026-01-13 17:45:55 -0500

    test: update main.go to pass parser output to analyser and resolver

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"rahu/analyser"
 	"rahu/parser"
 	"rahu/utils"
 )
@@ -14,4 +15,35 @@ func main() {
 	p := parser.New(input) // Pass the same input
 	module := p.Parse()
 	utils.PrintAST(os.Stdout, module)
+
+	fmt.Println("\nFirst pass, scopes: ")
+	global := analyser.BuildScopes(module)
+	utils.PrintScopes(os.Stdout, global)
+
+	fmt.Println("\nSecond pass, resolution: ")
+
+	errors, resolved := analyser.Resolve(module, global)
+
+	if len(errors) == 0 {
+		fmt.Println("No semantic errors")
+	} else {
+		for _, err := range errors {
+			fmt.Printf("Error at %v: %s\n", err.Span, err.Msg)
+		}
+	}
+
+	fmt.Println("\nResolved names")
+	for name, sym := range resolved {
+		fmt.Printf(
+			"USE %-10s %-12s at %d:%d  →  DEF %-10s %-12s at %d:%d\n",
+			name.ID,
+			sym.Kind,
+			name.Pos.Start.Line,
+			name.Pos.Start.Col,
+			sym.Name,
+			sym.Kind,
+			sym.Span.Start.Line,
+			sym.Span.Start.Col,
+		)
+	}
 }
