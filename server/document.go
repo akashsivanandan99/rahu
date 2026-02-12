5c22ce86fcd16b8cb335872959dad00e5bc1200f
Akash <akashsivanandan@protonmail.com>
2026-02-12 13:18:14 -0500

    feat: specify definition povidr capabilities on initialisation

--- server/document.go
+++ server/document.go
@@ -184,8 +184,9 @@ func (s *Server) Initialize(
 
 	return &lsp.InitializeResult{
 		Capabilities: lsp.ServerCapabilities{
-			TextDocumentSync: lsp.TDSKFull,
-			HoverProvider:    true,
+			TextDocumentSync:   lsp.TDSKFull,
+			HoverProvider:      true,
+			DefinitionProvider: true,
 		},
 	}, nil
 }
